130a0d2526ca9e02b872acd1ad0801bd4b880082
Dangelesm <davidangeles@itesm.mx>
2016-12-26 11:05:11 -0600

    Update Contents.swift

--- Velocimetro.playground/Contents.swift
+++ Velocimetro.playground/Contents.swift
@@ -2,20 +2,31 @@
  Playground: Representar un velocímetro de un automóvil
  Autor: David Angeles
  Fecha: 25/12/16
- GitHub: https://github.com/Dangelesm/MiniRetoRango0a100.git
+ GitHub: https://github.com/Dangelesm/MiniReto2.git
  ************************************/
 
 import UIKit
 
 class Auto{
-    var velocidad = velocidades()
+    var velocidad: velocidades
     
-    init(velocidadInicial:velocidades){
-        self.velocidad = velocidadInicial
+    init(){
+        self.velocidad = velocidades(velocidadInicial: .apagado)
     }
     
     func cambioDeVelocidad() ->(actual : Int, velodadEnCadena: String){
-        
+        let velocidadActual = self.velocidad
+        switch(self.velocidad){
+        case .apagado:
+            self.velocidad = .velocidadBaja
+        case .velocidadBaja:
+            self.velocidad = .velocidadMedia
+        case .velocidadMedia:
+            self.velocidad = .velocidadAlta
+        case .velocidadAlta:
+            self.velocidad = .velocidadMedia
+        }
+        return (velocidadActual.rawValue, velocidadActual.resultado)
     }
 }
 
@@ -25,7 +36,24 @@ enum velocidades: Int{
          velocidadMedia = 50,
          velocidadAlta = 120
     
-    init(){
-        self = .apagado
+    init(velocidadInicial:velocidades){
+        self = velocidadInicial
     }
+    
+    var resultado : String {
+        switch self {
+        case .apagado: return "Velocidad: APAGADO";
+        case .velocidadBaja: return "Velocidad: BAJA";
+        case .velocidadMedia: return "Velocidad: MEDIA";
+        case .velocidadAlta: return "Velocidad: ALTA";
+        }
+    }
+}
+
+//Prueba del Reto
+let Velocimetro: Auto = Auto()
+Velocimetro.velocidad
+for velocidades in 1...20{
+    let respuesta = Velocimetro.cambioDeVelocidad()
+    print(velocidades,"\t",respuesta.actual, "\t", respuesta.velodadEnCadena)
 }
